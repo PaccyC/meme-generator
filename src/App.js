7f8831f99831025791a50b7fdbd6959fee458f05
PaccyC <abayisengaaimepacifique@gmail.com>
2023-04-22 15:47:28 +0000

    initializing meme-generator-app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Header from "./components/Header";
+import Form from "./components/Form";
+
+
+function App() {
+
+  return (
+    <div className="App">
+
+      <Header/>
+      
+      <Form/>
+    </div>
+  );
+}
+
+export default App;
